9e1ae300759c038541d3f4caa0f5a8613435bb16
Alexandr <alexandrzhdanov25@mail.ru>
2022-10-28 18:31:28 +0300

    relase v1

---
+++ trie.py
@@ -0,0 +1,32 @@
+class Trie:
+
+    def __init__(self, extension):
+        self.root = {}
+        self.extension = extension
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            node = node.setdefault(char, {})    
+        node['key'] = True                      
+
+    def search(self, word: str) -> bool:
+
+        node = self.root                
+
+        for char in word:
+                if char in node:                    
+                    node = node[char]               
+                else:                               
+                    if char !='\n':
+                        return False
+
+        return True if 'key' in node else False
+
+
+
+        
+
+
+
+        
